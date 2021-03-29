6ad8220e22cb32508530e2564c8c28336519ebdb
eksrha <58111764+eksrha@users.noreply.github.com>
2021-03-29 19:25:54 +0200

    ...

---
+++ gitcontroller/initsenv.go
@@ -0,0 +1,62 @@
+package gitcontroller
+
+import (
+	"fmt"
+	"os"
+)
+
+func getEnv(key, fallback string, confidential bool) string {
+	if value, ok := os.LookupEnv(key); ok {
+		var newValue = "**********"
+		if len(value) > 10 {
+			newValue = value[0:4] + "******"
+		}
+		return newValue
+	}
+	return fallback
+}
+
+func determanEnvironment(environment string) GitConfiguration {
+	var conf GitConfiguration
+	switch environment {
+	case "Github":
+		return GitConfiguration{
+			Hostname:    os.Getenv("GITHUB_SERVER_URL"),
+			ApiVersion:  getEnv("GIT_API_VERSION", "v3", false),
+			Actor:       os.Getenv("GITHUB_ACTOR"),
+			Repository:  os.Getenv("GITHUB_REPOSITORY"),
+			AccessToken: os.Getenv("GIT_ACCESS_TOKEN"),
+		}
+
+	case "Custom":
+		fmt.Println("In order to establish a connection to the github please provide the following environment variables:")
+		fmt.Printf("   GIT_HOSTNAME      %s\n", getEnv("GIT_HOSTNAME", "git.daimler.com", false))
+		fmt.Printf("   GIT_API_VERSION   %s\n", getEnv("GIT_API_VERSION", "v3", false))
+		fmt.Printf("   GIT_ORGA          %s\n", getEnv("GIT_ORGA", "not-set", false))
+		fmt.Printf("   GIT_REPO          %s\n", getEnv("GIT_REPO", "not-set", false))
+		fmt.Printf("   GIT_ACCESS_TOKEN  %s\n", getEnv("GIT_ACCESS_TOKEN", "not-set", true))
+	}
+
+	return conf
+}
+
+// GetLatestReleaseVersion
+func GetLatestReleaseVersion(environment string) string {
+
+	switch environment {
+	case "Github":
+		conf := determanEnvironment(environment)
+		return github_getLatestReleaseVersion(conf)
+	}
+	return ""
+}
+
+// GetLatestReleaseVersion
+func CreateNextGitHubRelease(environment string, newReleaseVersion string) {
+
+	switch environment {
+	case "Github":
+		conf := determanEnvironment(environment)
+		github_createNextGitHubRelease(conf, newReleaseVersion)
+	}
+}
