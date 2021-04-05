8aadc570d561a3a667062bb78a2fad5baca74d7e
eksrha <58111764+eksrha@users.noreply.github.com>
2021-04-05 18:34:46 +0200

    zwischenstand rewrite service

--- main.go
+++ main.go
@@ -1,103 +1,63 @@
 package main
 
 import (
-	"awesome-ci-semver/gitcontroller"
-	"awesome-ci-semver/semver"
+	"awesome-ci-semver/service"
 	"flag"
-	"fmt"
 	"os"
-	"os/exec"
-	"regexp"
 )
 
-func runcmd(cmd string, shell bool) string {
-	if shell {
-		out, err := exec.Command("bash", "-c", cmd).Output()
-		if err != nil {
-			fmt.Println(err)
-			panic("some error found")
-		}
-		return string(out)
-	}
-	out, err := exec.Command(cmd).Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(out)
+var (
+	cienv         *string
+	createRelease CreateReleaseSet
+	getBuildInfos getBuildInfosSet
+)
+
+type CreateReleaseSet struct {
+	fs         *flag.FlagSet
+	version    string
+	patchLevel string
+	publishNpm string
+	dryRun     bool
 }
 
-func isFlagPassed(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
+type getBuildInfosSet struct {
+	fs         *flag.FlagSet
+	version    string
+	patchLevel string
+	output     string
 }
 
-func funcCreateRelease(cienv *string, overrideVersion *string, getVersionIncrease *string, isDryRun *bool) {
-	var gitVersion string
-	if *overrideVersion != "" {
-		gitVersion = *overrideVersion
-	} else {
-		gitVersion = gitcontroller.GetLatestReleaseVersion(*cienv)
-	}
+func init() {
+	cienv = flag.String("cienv", "Github", "set your CI Environment for Special Featueres!\nAvalible: Jenkins, Github, Gitlab, Custom\nDefault: Github")
 
-	var patchLevel string
-	if *getVersionIncrease != "" {
-		patchLevel = *getVersionIncrease
-	} else {
-		if *cienv == "Github" {
-			// Output: []string {FullString, PR, FullBranch, Orga, branch, branchBegin, restOfBranch}
-			regex := `[a-zA-z ]+#([0-9]+) from (([0-9a-zA-Z-]+)/((feature|bugfix|fix)/(.+)))`
-			r := regexp.MustCompile(regex)
+	// createReleaseSet
+	createRelease.fs = flag.NewFlagSet("createRelease", flag.ExitOnError)
+	createRelease.fs.StringVar(&createRelease.version, "version", "", "override version to Update")
+	createRelease.fs.StringVar(&createRelease.patchLevel, "patchLevel", "bugfix", "predefine version to Update")
+	createRelease.fs.StringVar(&createRelease.publishNpm, "publishNpm", "", "runs npm publish --tag <createdTag> with custom directory")
+	createRelease.fs.BoolVar(&createRelease.dryRun, "dry-run", false, "make dry-run before writing version to Git")
 
-			// mergeMessage := r.FindStringSubmatch(`Merge pull request #3 from ITC-TO-MT/feature/test-1`)
-			mergeMessage := r.FindStringSubmatch(runcmd(`git log -1 --pretty=format:"%s"`, true))
-			if len(mergeMessage) > 0 {
-				fmt.Printf("PR-Number: %s\n", mergeMessage[1])
-				fmt.Printf("Merged branch is a %s\n", mergeMessage[5])
-				patchLevel = mergeMessage[5]
-			} else {
-				fmt.Println("No merge message found pls make shure this regex matches: ", regex)
-				fmt.Print("Example: Merge pull request #3 from some-orga/feature/awesome-feature\n\n")
-				fmt.Print("If you like to set your patch level manually by flag: -level (feautre|bugfix)\n\n")
-				os.Exit(1)
-			}
-		}
-	}
-
-	newVersion := semver.IncreaseSemVer(patchLevel, gitVersion)
-	if *isDryRun {
-		fmt.Printf("Old version: %s\n", gitVersion)
-		fmt.Printf("Would writing new release: %s\n", newVersion)
-	} else {
-		fmt.Printf("Old version: %s\n", gitVersion)
-		fmt.Printf("Writing new release: %s\n", newVersion)
-		gitcontroller.CreateNextGitHubRelease(*cienv, newVersion)
-	}
+	// getNewReleaseVersion
+	getBuildInfos.fs = flag.NewFlagSet("getBuildInfos", flag.ExitOnError)
+	getBuildInfos.fs.StringVar(&getBuildInfos.version, "version", "", "override version to Update")
+	getBuildInfos.fs.StringVar(&getBuildInfos.patchLevel, "patchLevel", "bugfix", "predefine version to Update")
+	getBuildInfos.fs.StringVar(&getBuildInfos.output, "output", "pr,version", "define output by get")
 }
 
 func main() {
 
-	cienv := flag.String("cienv", "Github", "set your CI Environment for Special Featueres!\nAvalible: Jenkins, Github, Gitlab, Custom\nDefault: Github")
-
-	createRelease := flag.NewFlagSet("createRelease", flag.ExitOnError)
-	overrideVersion := createRelease.String("version", "", "override version to Update")
-	getVersionIncrease := createRelease.String("level", "", "predefine version to Update")
-	isDryRun := createRelease.Bool("dry-run", false, "Make dry-run before writing version to Git")
+	// disable logging
+	//log.SetOutput(ioutil.Discard)
 
 	flag.Parse()
 
-	if os.Args[1] != "createRelease" {
-		fmt.Println("expected 'createRelease', '...' or 'bar' subcommands")
-		os.Exit(1)
-	}
-
 	switch os.Args[1] {
 	case "createRelease":
-		createRelease.Parse(os.Args[2:])
-		funcCreateRelease(cienv, overrideVersion, getVersionIncrease, isDryRun)
+		createRelease.fs.Parse(os.Args[2:])
+		service.CreateRelease(cienv, &createRelease.version, &createRelease.patchLevel, &createRelease.dryRun, &createRelease.publishNpm)
+	case "getBuildInfos":
+		getBuildInfos.fs.Parse(os.Args[2:])
+		service.GetBuildInfos(cienv, &getBuildInfos.version, &getBuildInfos.patchLevel, &getBuildInfos.output)
 	}
+
 }
---
+++ service/createRelease.go
@@ -0,0 +1,117 @@
+package service
+
+import (
+	"awesome-ci-semver/gitcontroller"
+	"awesome-ci-semver/semver"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func runcmd(cmd string, shell bool) string {
+	if shell {
+		out, err := exec.Command("bash", "-c", cmd).Output()
+		if err != nil {
+			fmt.Println(err)
+			panic("some error found")
+		}
+		return string(out)
+	}
+	out, err := exec.Command(cmd).Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(out)
+}
+
+func CreateRelease(cienv *string, overrideVersion *string, getVersionIncrease *string, isDryRun *bool, publishNpm *string) {
+	var gitVersion string
+	if *overrideVersion != "" {
+		gitVersion = *overrideVersion
+	} else {
+		gitVersion = gitcontroller.GetLatestReleaseVersion(*cienv)
+	}
+
+	var patchLevel string
+	if *getVersionIncrease != "" {
+		patchLevel = *getVersionIncrease
+	} else {
+		if *cienv == "Github" {
+			// Output: []string {FullString, PR, FullBranch, Orga, branch, branchBegin, restOfBranch}
+			regex := `[a-zA-z ]+#([0-9]+) from (([0-9a-zA-Z-]+)/((feature|bugfix|fix)/(.+)))`
+			r := regexp.MustCompile(regex)
+
+			// mergeMessage := r.FindStringSubmatch(`Merge pull request #3 from ITC-TO-MT/feature/test-1`)
+			mergeMessage := r.FindStringSubmatch(runcmd(`git log -1 --pretty=format:"%s"`, true))
+			if len(mergeMessage) > 0 {
+				fmt.Printf("PR-Number: %s\n", mergeMessage[1])
+				fmt.Printf("Merged branch is a %s\n", mergeMessage[5])
+				patchLevel = mergeMessage[5]
+			} else {
+				fmt.Println("No merge message found pls make shure this regex matches: ", regex)
+				fmt.Print("Example: Merge pull request #3 from some-orga/feature/awesome-feature\n\n")
+				fmt.Print("If you like to set your patch level manually by flag: -level (feautre|bugfix)\n\n")
+				os.Exit(1)
+			}
+		}
+	}
+
+	newVersion := semver.IncreaseSemVer(patchLevel, gitVersion)
+	if *isDryRun {
+		fmt.Printf("Old version: %s\n", gitVersion)
+		fmt.Printf("Would writing new release: %s\n", newVersion)
+	} else {
+		fmt.Printf("Old version: %s\n", gitVersion)
+		fmt.Printf("Writing new release: %s\n", newVersion)
+		gitcontroller.CreateNextGitHubRelease(*cienv, newVersion)
+	}
+
+	if *publishNpm != "" {
+		// check if subfolder has slash
+		pathToSource := *publishNpm
+		if !strings.HasSuffix(*publishNpm, "/") {
+			pathToSource = *publishNpm + "/"
+		}
+		fmt.Printf("Puplishing npm packages under path: %s\n", pathToSource)
+		npmPublish(pathToSource, newVersion)
+	}
+}
+
+func npmPublish(pathToSource string, newVersion string) {
+
+	// opening package.json
+	jsonFile, err := os.Open(pathToSource + "package.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	defer jsonFile.Close()
+
+	var result map[string]interface{}
+	json.NewDecoder(jsonFile).Decode(&result)
+
+	result["version"] = newVersion
+
+	b, err := json.MarshalIndent(result, "", " ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	// writing result to package.json
+	err = ioutil.WriteFile(pathToSource+"package.json", b, 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	command := exec.Command("npm", "publish", pathToSource, "--tag", fmt.Sprintf("%s@%s", result["name"], newVersion))
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	// Run the command
+	command.Run()
+}
