becb355cdf9295dd3560569ee23c252326879450
EIKE SOEREN VOM BAUER <eike_soeren.vom_bauer@daimler.com>
2021-04-09 08:44:20 +0200

    fix infosMergeMessage

--- service/service.go
+++ service/service.go
@@ -32,17 +32,15 @@ func getMergeMessage() (infos infosMergeMessage, err error) {
 	regex := `[a-zA-z ]+#([0-9]+) from (([0-9a-zA-Z-]+)/((feature|bugfix|fix)/(.+)))`
 	r := regexp.MustCompile(regex)
 
-	// var infos infosMergeMessage
-
-	// mergeMessage := r.FindStringSubmatch(`Merge pull request #3 from ITC-TO-MT/feature/test-1`)
+	// mergeMessage := r.FindStringSubmatch(`Merge pull request #3 from test-orga/feature/test-1`)
 	mergeMessage := r.FindStringSubmatch(runcmd(`git log -1 --pretty=format:"%s"`, true))
 	if len(mergeMessage) > 0 {
 		infos.PRNumber = mergeMessage[1]
 		infos.PatchLevel = mergeMessage[5]
+		return infos, nil
 	} else {
 		return infos, errors.New("No merge message found pls make shure this regex matches: " + regex +
 			"\nExample: Merge pull request #3 from some-orga/feature/awesome-feature" +
 			"\nIf you like to set your patch level manually by flag: -level (feautre|bugfix)")
 	}
-	return infos, nil
 }
