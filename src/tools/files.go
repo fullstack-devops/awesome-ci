099fda21a063d9532c1f683e255db1efd7bebe9e
eksrha <58111764+eksrha@users.noreply.github.com>
2022-01-30 18:37:12 +0100

    add body message + asset testing before upload

---
+++ src/tools/files.go
@@ -0,0 +1,83 @@
+package tools
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type UploadArtifact struct {
+	File os.File
+	Name string
+}
+
+type UploadAsset struct {
+	File  os.File
+	Name  string
+	IsZip bool
+}
+
+func GetFilesAndInfos(uploadArtifacts *string) (artifacts []UploadArtifact, err error) {
+	artifactsToUpload := strings.Split(*uploadArtifacts, ",")
+	for _, artifact := range artifactsToUpload {
+		var sanFilename string
+		if strings.HasPrefix(artifact, "file=") {
+			sanFilename = artifact[5:]
+		}
+		file, err := os.OpenFile(sanFilename, os.O_RDWR, 0644)
+		if err != nil {
+			return nil, err
+		}
+		info, _ := file.Stat()
+		artifacts = append(artifacts, UploadArtifact{
+			File: *file,
+			Name: info.Name(),
+		})
+	}
+	return
+}
+
+func GetAsstes(uploadArtifacts *string, check bool) (asset []UploadAsset, err error) {
+	artifactsToUpload := strings.Split(*uploadArtifacts, ",")
+	for _, artifact := range artifactsToUpload {
+		assetName := strings.Split(artifact, "=")
+
+		switch assetName[0] {
+		case "file":
+			file, err := os.OpenFile(assetName[1], os.O_RDWR, 0644)
+			if err != nil {
+				return nil, err
+			}
+			info, err := file.Stat()
+			if err != nil {
+				return nil, err
+			}
+			// if check, then close file, else return file interface for upload
+			if check {
+				file.Close()
+			} else {
+				asset = append(asset, UploadAsset{
+					File:  *file,
+					Name:  info.Name(),
+					IsZip: false,
+				})
+			}
+		case "files":
+			// todo
+		case "zip":
+			// todo
+		default:
+			return nil, fmt.Errorf("not an valid asset format")
+		}
+	}
+
+	return
+}
+
+func CheckIsFile(name string) (body string, err error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
