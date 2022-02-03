d1899acec99bd20a7d71672f8d9f5233f91e76f5
eksrha <58111764+eksrha@users.noreply.github.com>
2022-02-03 09:31:43 +0100

    Feature/pr comments issue 70 (#73)

--- src/acigithub/pullrequest.go
+++ src/acigithub/pullrequest.go
@@ -6,6 +6,8 @@ import (
 	"awesome-ci/src/tools"
 	"fmt"
 	"log"
+	"os"
+	"regexp"
 	"strings"
 
 	"github.com/google/go-github/v39/github"
@@ -53,11 +55,23 @@ func GetPrInfos(prNumber int, mergeCommitSha string) (standardPrInfos *models.St
 		return nil, nil, fmt.Errorf("no pull request found, please check if all resources are specified")
 	}
 
+	isCI, isCIBool := os.LookupEnv("CI")
+	_, isSilentBool := os.LookupEnv("ACI_SILENT")
+	if isCIBool && !isSilentBool {
+		if *prInfos.State == "open" && isCI == "true" {
+			err = CommentHelpToPullRequest(*prInfos.Number)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
 	prSHA := *prInfos.Head.SHA
 	branchName := *prInfos.Head.Ref
 	patchLevel := branchName[:strings.Index(branchName, "/")]
 
-	// if an comment exists with aci=major, make a major version!
+	var version = ""
+	// if an comment exists with aci_patch_level=major, make a major version!
 	issueComments, err := GetIssueComments(prNumber, owner, repo)
 	if err != nil {
 		return nil, nil, err
@@ -65,9 +79,18 @@ func GetPrInfos(prNumber int, mergeCommitSha string) (standardPrInfos *models.St
 	for _, comment := range issueComments {
 		// Must have permission in the repo to create a major version
 		// MANNEQUIN|NONE https://docs.github.com/en/graphql/reference/enums#commentauthorassociation
-		if strings.Contains(*comment.Body, "aci=major") && strings.Contains("OWNER|CONTRIBUTOR|COLLABORATOR", *comment.AuthorAssociation) {
-			patchLevel = "major"
-			break
+		if strings.Contains("OWNER|CONTRIBUTOR|COLLABORATOR", *comment.AuthorAssociation) {
+			aciVersionOverride := regexp.MustCompile(`aci_version_override: ([0-9]+\.[0-9]+\.[0-9]+)`)
+			aciPatchLevel := regexp.MustCompile(`aci_patch_level: ([a-zA-Z]+)`)
+
+			if aciPatchLevel.MatchString(*comment.Body) {
+				patchLevel = aciVersionOverride.FindStringSubmatch(*comment.Body)[1]
+				break
+			}
+			if aciVersionOverride.MatchString(*comment.Body) {
+				version = aciVersionOverride.FindStringSubmatch(*comment.Body)[1]
+				break
+			}
 		}
 	}
 
@@ -75,9 +98,12 @@ func GetPrInfos(prNumber int, mergeCommitSha string) (standardPrInfos *models.St
 	if err != nil {
 		return nil, nil, err
 	}
-	nextVersion, err := semver.IncreaseVersion(patchLevel, *repositoryRelease.TagName)
-	if err != nil {
-		return nil, nil, err
+
+	if version == "" {
+		version, err = semver.IncreaseVersion(patchLevel, *repositoryRelease.TagName)
+		if err != nil {
+			return nil, nil, err
+		}
 	}
 
 	standardPrInfos = &models.StandardPrInfos{
@@ -90,7 +116,7 @@ func GetPrInfos(prNumber int, mergeCommitSha string) (standardPrInfos *models.St
 		PatchLevel:     patchLevel,
 		LatestVersion:  *repositoryRelease.TagName,
 		CurrentVersion: "",
-		NextVersion:    nextVersion,
+		NextVersion:    version,
 		MergeCommitSha: *prInfos.MergeCommitSHA,
 	}
 	return
